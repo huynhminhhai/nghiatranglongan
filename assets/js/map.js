1b2ccc6be5f93a93307a82dd34f104ee48264119
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-12-11 16:17:00 +0700

    update layer zoom and rotate

--- assets/js/map.js
+++ assets/js/map.js
@@ -26,16 +26,35 @@ $(document).ready(function () {
     };
 
     // Tạo bản đồ
-    const map = L.map('map').setView([data.center_lat, data.center_lng], data.zoom);
+    const map = L.map('map', {
+        rotate: true,
+        rotateControl: {
+            closeOnZeroBearing: false,
+            // position: 'bottomleft',
+        },
+        touchRotate: true,
+        bearing: 54,
+    }).setView([data.center_lat, data.center_lng], 19);
 
     // Lớp bản đồ đường xá
     const streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
+        attribution: '',
+        maxZoom: data.zoom,
+        zoom: 19
     });
 
     // Lớp bản đồ vệ tinh (sử dụng Esri)
-    const satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles © Esri & the GIS User Community'
+    const satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+        attribution: '',
+        zoom: 19,
+        maxZoom: data.zoom,
+    });
+
+    const whiteBackground = L.tileLayer('', {
+        attribution: '',
+        maxZoom: data.zoom,
+        zoom: 19
     });
 
     // Thêm lớp bản đồ mặc định
@@ -44,7 +63,8 @@ $(document).ready(function () {
     // Thêm Layer Control
     L.control.layers({
         "Giao thông": streets,
-        "Vệ tinh": satellite
+        "Vệ tinh": satellite,
+        "Nền trắng": whiteBackground
     }).addTo(map);
 
 
@@ -52,6 +72,10 @@ $(document).ready(function () {
     L.tileLayer('', {
         attribution: '© VNPT Long An'
     }).addTo(map);
+
+
+    // Bỏ tính năng double click zoom
+    map.doubleClickZoom.disable();
 
     // Thêm marker cho từng khu vực
     data.listKhu.forEach(khu => {
@@ -61,5 +85,20 @@ $(document).ready(function () {
         });
 
         L.marker([khu.lat, khu.lng], { icon: icon }).addTo(map);
+    });
+
+    let markers = []; // Mảng lưu trữ các marker
+
+    // Xử lý double click để thêm điểm mới
+    map.on('dblclick', function (e) {
+        const latlng = e.latlng; // Lấy tọa độ của điểm click
+
+        // Xóa các marker cũ
+        markers.forEach(marker => map.removeLayer(marker));
+        markers = []; // Reset mảng
+
+        // Thêm marker mới
+        const newMarker = L.marker([latlng.lat, latlng.lng]).addTo(map);
+        markers.push(newMarker); // Lưu vào mảng
     });
 })
