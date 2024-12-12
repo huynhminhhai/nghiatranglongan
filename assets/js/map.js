289159360d65983400c4da6c4cb49f838bb3d634
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-12-12 07:37:27 +0700

    add marker

--- assets/js/map.js
+++ assets/js/map.js
@@ -89,8 +89,33 @@ $(document).ready(function () {
 
     let markers = []; // Mảng lưu trữ các marker
 
+    const defaultMarker = L.marker([10.562402692690446, 106.41901774627047], { icon: L.divIcon({
+        className: 'text-label',
+        html: `
+            <div style="background-color: #fff;height: 30px;width: 30px;display: flex;align-items: center;justify-content: center;border-radius: 100%;position: relative;transform: translate(-9px, -8px);">
+                <svg style="transform: rotate(-45deg) translateX(-2px) translateY(2px);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="#a01c10" d="M20.891 2.006L20.997 2l.13.008l.09.016l.123.035l.107.046l.1.057l.09.067l.082.075l.052.059l.082.116l.052.096q.07.15.09.316l.005.106q0 .113-.024.22l-.035.123l-6.532 18.077A1.55 1.55 0 0 1 14 22.32a1.55 1.55 0 0 1-1.329-.747l-.065-.127l-3.352-6.702l-6.67-3.336a1.55 1.55 0 0 1-.898-1.259L1.68 10c0-.56.301-1.072.841-1.37l.14-.07l18.017-6.506l.106-.03l.108-.018z" />
+                </svg>
+            </div>    
+        `
+    }) }).addTo(map);
+
+    markers.push(defaultMarker);
+
     // Xử lý double click để thêm điểm mới
     map.on('dblclick', function (e) {
+
+        const icon = L.divIcon({
+            className: 'text-label',
+            html: `
+                <div style="background-color: #fff;height: 30px;width: 30px;display: flex;align-items: center;justify-content: center;border-radius: 100%;position: relative;transform: translate(-9px, -8px);">
+                    <svg style="transform: rotate(-45deg) translateX(-2px) translateY(2px);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+	                    <path fill="#a01c10" d="M20.891 2.006L20.997 2l.13.008l.09.016l.123.035l.107.046l.1.057l.09.067l.082.075l.052.059l.082.116l.052.096q.07.15.09.316l.005.106q0 .113-.024.22l-.035.123l-6.532 18.077A1.55 1.55 0 0 1 14 22.32a1.55 1.55 0 0 1-1.329-.747l-.065-.127l-3.352-6.702l-6.67-3.336a1.55 1.55 0 0 1-.898-1.259L1.68 10c0-.56.301-1.072.841-1.37l.14-.07l18.017-6.506l.106-.03l.108-.018z" />
+                    </svg>
+                </div>    
+            `
+        });
+
         const latlng = e.latlng; // Lấy tọa độ của điểm click
 
         // Xóa các marker cũ
@@ -98,7 +123,9 @@ $(document).ready(function () {
         markers = []; // Reset mảng
 
         // Thêm marker mới
-        const newMarker = L.marker([latlng.lat, latlng.lng]).addTo(map);
+        const newMarker = L.marker([latlng.lat, latlng.lng], { icon: icon }).addTo(map);
         markers.push(newMarker); // Lưu vào mảng
+
+        console.log(newMarker)
     });
 })
